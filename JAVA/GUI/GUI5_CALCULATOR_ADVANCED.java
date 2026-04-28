6965a7c3bdb5ba96c500725b48dd5f18ef3347b4
JAS2343L <js9404107@gmail.com>
2026-04-28 15:11:00 +0530

    added plus, changed all number's font side to 22, Thats it...

--- JAVA/GUI/GUI5_CALCULATOR_ADVANCED.java
+++ JAVA/GUI/GUI5_CALCULATOR_ADVANCED.java
@@ -4,8 +4,11 @@ import java.awt.event.*;
 import java.awt.Font;
 
 public class GUI5_CALCULATOR_ADVANCED {
+
+    static int number1 = 0;
     
       GUI5_CALCULATOR_ADVANCED() {
+
 
         //New Window:
 
@@ -32,48 +35,73 @@ public class GUI5_CALCULATOR_ADVANCED {
         JButton b7 = new JButton("7");
         b7.setBounds(30, 280, 70, 70);
         b7.addActionListener(e -> txtfield.setText(txtfield.getText()+"7"));
+        b7.setFont(new Font("ubuntu", Font.BOLD, 22));
         
         JButton b8 = new JButton("8");
         b8.setBounds(110, 280, 70, 70);
         b8.addActionListener(e -> txtfield.setText(txtfield.getText()+"8"));
+        b8.setFont(new Font("ubuntu", Font.BOLD, 22));
 
         JButton b9 = new JButton("9");
         b9.setBounds(190, 280, 70, 70);
         b9.addActionListener(e -> txtfield.setText(txtfield.getText()+"9"));
+        b9.setFont(new Font("ubuntu", Font.BOLD, 22));
 
         JButton b4 = new JButton("4");
         b4.setBounds(30, 360, 70, 70);
         b4.addActionListener(e -> txtfield.setText(txtfield.getText()+"4"));
+        b4.setFont(new Font("ubuntu", Font.BOLD, 22));
 
         JButton b5 = new JButton("5");
         b5.setBounds(110, 360, 70, 70);
         b5.addActionListener(e -> txtfield.setText(txtfield.getText()+"5"));
+        b5.setFont(new Font("ubuntu", Font.BOLD, 22));
 
         JButton b6 = new JButton("6");
         b6.setBounds(190, 360, 70, 70);
         b6.addActionListener(e -> txtfield.setText(txtfield.getText()+"6"));
+        b6.setFont(new Font("ubuntu", Font.BOLD, 22));
 
         JButton b1 = new JButton("1");
         b1.setBounds(30, 440, 70, 70);
         b1.addActionListener(e -> txtfield.setText(txtfield.getText()+"1"));
+        b1.setFont(new Font("ubuntu", Font.BOLD, 22));
 
         JButton b2 = new JButton("2");
         b2.setBounds(110, 440, 70, 70);
         b2.addActionListener(e -> txtfield.setText(txtfield.getText()+"2"));
+        b2.setFont(new Font("ubuntu", Font.BOLD, 22));
 
         JButton b3 = new JButton("3");
         b3.setBounds(190, 440, 70, 70);
         b3.addActionListener(e -> txtfield.setText(txtfield.getText()+"3"));
+        b3.setFont(new Font("ubuntu", Font.BOLD, 22));
 
         JButton b0 = new JButton("0");
         b0.setBounds(110, 520, 70, 70);
         b0.addActionListener(e -> txtfield.setText(txtfield.getText()+"0"));
+        b0.setFont(new Font("ubuntu", Font.BOLD, 22));
 
-        //clear all, Button:
+        //Operators
+        JButton plus = new JButton("+");
+        plus.setBounds(280, 200, 70, 70);
+        plus.setFont(new Font("ubuntu", Font.BOLD, 22));
+
+
+        //clear, Buttons:
 
         JButton c = new JButton("C");
         c.setBounds(30, 200, 70, 70);
         c.addActionListener(e -> txtfield.setText(""));
+        c.setFont(new Font("ubuntu", Font.BOLD, 22));
+
+        JButton backspace = new JButton ("⌫");
+        backspace.setBounds(110, 200, 70, 70);;
+        backspace.addActionListener(e -> {
+            if (txtfield.getText().length() > 0){
+                txtfield.setText(txtfield.getText().substring(0, txtfield.getText().length() -1));
+            }
+        });
 
 
 
@@ -81,9 +109,10 @@ public class GUI5_CALCULATOR_ADVANCED {
         //adding all in one window:
 
         gui.add(txtfield);
-        gui.add(c);
 
-        gui.add(b7);gui.add(b8);gui.add(b9);
+
+        gui.add(c) ;gui.add(backspace);        gui.add(plus);
+        gui.add(b7);gui.add(b8);gui.add(b9);  //gui.add();
         gui.add(b4);gui.add(b5);gui.add(b6);
         gui.add(b3);gui.add(b2);gui.add(b1);
                     gui.add(b0);
