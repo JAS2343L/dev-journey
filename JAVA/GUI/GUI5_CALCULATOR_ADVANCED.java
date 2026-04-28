71709d7a5dbaa26389bdc74faa406358f453b277
JAS2343L <js9404107@gmail.com>
2026-04-28 10:58:21 +0530

    Now, i placed calculator functions outside the main method and called i from main method, added button 7,8,9,4,5,6,1,2,3,0 changed format from 1,2,3,4 to 9,8,7,6. added my name!!, added comments for understanding the code. CHANGED THE size of window from 400, 600 to 370, 620 for perfection.. THATS IT....

--- JAVA/GUI/GUI5_CALCULATOR_ADVANCED.java
+++ JAVA/GUI/GUI5_CALCULATOR_ADVANCED.java
@@ -4,45 +4,102 @@ import java.awt.event.*;
 import java.awt.Font;
 
 public class GUI5_CALCULATOR_ADVANCED {
-    public static void main(String[] args) {
+    
+      GUI5_CALCULATOR_ADVANCED() {
+
+        //New Window:
+
         JFrame gui = new JFrame("calculator");
 
-        JTextField tfld = new JTextField();
-        tfld.setBounds(60, 100, 280, 60);
-        tfld.setFont(new Font("ubuntu", Font.BOLD , 29));
-        tfld.setEditable(false);
-        tfld.setHorizontalAlignment(JTextField.RIGHT);
+
+        //Text Field:
+
+        JTextField txtfield = new JTextField();
+        txtfield.setBounds(44, 100, 280, 60);
+        txtfield.setFont(new Font("ubuntu", Font.BOLD , 29));
+        txtfield.setEditable(false);
+        txtfield.setHorizontalAlignment(JTextField.RIGHT);
+
+        //My name:
+
+        JLabel me = new JLabel("Made By Jasmeet(JAS2343L)");
+        me.setBounds(90, 560, 200, 99);
+        me.setFont(new Font("ubuntu", Font.BOLD, 13));
+
+
+        //buttons:
+
+        JButton b7 = new JButton("7");
+        b7.setBounds(30, 280, 70, 70);
+        b7.addActionListener(e -> txtfield.setText(txtfield.getText()+"7"));
+        
+        JButton b8 = new JButton("8");
+        b8.setBounds(110, 280, 70, 70);
+        b8.addActionListener(e -> txtfield.setText(txtfield.getText()+"8"));
+
+        JButton b9 = new JButton("9");
+        b9.setBounds(190, 280, 70, 70);
+        b9.addActionListener(e -> txtfield.setText(txtfield.getText()+"9"));
+
+        JButton b4 = new JButton("4");
+        b4.setBounds(30, 360, 70, 70);
+        b4.addActionListener(e -> txtfield.setText(txtfield.getText()+"4"));
+
+        JButton b5 = new JButton("5");
+        b5.setBounds(110, 360, 70, 70);
+        b5.addActionListener(e -> txtfield.setText(txtfield.getText()+"5"));
+
+        JButton b6 = new JButton("6");
+        b6.setBounds(190, 360, 70, 70);
+        b6.addActionListener(e -> txtfield.setText(txtfield.getText()+"6"));
 
         JButton b1 = new JButton("1");
-        b1.setBounds(30, 270, 70, 70);
-        b1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                tfld.setText(tfld.getText() + "1");
-            }
-        });
-        
+        b1.setBounds(30, 440, 70, 70);
+        b1.addActionListener(e -> txtfield.setText(txtfield.getText()+"1"));
+
         JButton b2 = new JButton("2");
-        b2.setBounds(100, 270, 70, 70);
-        b2.addActionListener(e -> tfld.setText(tfld.getText()+"2"));
+        b2.setBounds(110, 440, 70, 70);
+        b2.addActionListener(e -> txtfield.setText(txtfield.getText()+"2"));
 
         JButton b3 = new JButton("3");
-        b3.setBounds(170, 270, 70, 70);
-        b3.addActionListener(e -> tfld.setText(tfld.getText()+"3"));
+        b3.setBounds(190, 440, 70, 70);
+        b3.addActionListener(e -> txtfield.setText(txtfield.getText()+"3"));
+
+        JButton b0 = new JButton("0");
+        b0.setBounds(110, 520, 70, 70);
+        b0.addActionListener(e -> txtfield.setText(txtfield.getText()+"0"));
+
+        //clear all, Button:
 
         JButton c = new JButton("C");
         c.setBounds(30, 200, 70, 70);
-        c.addActionListener(e -> tfld.setText(""));
+        c.addActionListener(e -> txtfield.setText(""));
 
 
 
 
-        gui.add(tfld);
+        //adding all in one window:
+
+        gui.add(txtfield);
         gui.add(c);
-        gui.add(b1);gui.add(b2);gui.add(b3);
-        gui.setSize(400, 600);
+
+        gui.add(b7);gui.add(b8);gui.add(b9);
+        gui.add(b4);gui.add(b5);gui.add(b6);
+        gui.add(b3);gui.add(b2);gui.add(b1);
+                    gui.add(b0);
+
+        gui.add(me);
+
+        //Gui Window:
+
+        gui.setSize(370, 620);
         gui.setLayout(null);
         gui.setLocationRelativeTo(null);
         gui.setResizable(false);
         gui.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GUI5_CALCULATOR_ADVANCED();
     }
 }
