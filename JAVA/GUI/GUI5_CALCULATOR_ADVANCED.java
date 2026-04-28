5857677c42673009a24f606a3f712c1780dab7cf
JAS2343L <js9404107@gmail.com>
2026-04-28 10:20:33 +0530

    added b1, b2 with actionevent e. added "C" to clear textbox, added textbox with size 29 and ubuntu font. Thats it!

--- JAVA/GUI/GUI5_CALCULATOR_ADVANCED.java
+++ JAVA/GUI/GUI5_CALCULATOR_ADVANCED.java
@@ -1,24 +1,44 @@
 package GUI;
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.Font;
 
 public class GUI5_CALCULATOR_ADVANCED {
     public static void main(String[] args) {
         JFrame gui = new JFrame("calculator");
 
-        JButton a = new JButton("1");
-        a.setBounds(30, 270, 70, 70);
+        JTextField tfld = new JTextField();
+        tfld.setBounds(60, 100, 280, 60);
+        tfld.setFont(new Font("ubuntu", Font.BOLD , 29));
+        tfld.setEditable(false);
+        tfld.setHorizontalAlignment(JTextField.RIGHT);
 
-        JButton b = new JButton("2");
-        b.setBounds(110, 270, 70, 70);
+        JButton b1 = new JButton("1");
+        b1.setBounds(30, 270, 70, 70);
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                tfld.setText(tfld.getText() + "1");
+            }
+        });
+        
+        JButton b2 = new JButton("2");
+        b2.setBounds(100, 270, 70, 70);
+        b2.addActionListener(e -> tfld.setText(tfld.getText()+"2"));
 
-        JButton c = new JButton("3");
-        c.setBounds(190, 270, 70, 70);
+        JButton b3 = new JButton("3");
+        b3.setBounds(170, 270, 70, 70);
+        b3.addActionListener(e -> tfld.setText(tfld.getText()+"3"));
+
+        JButton c = new JButton("C");
+        c.setBounds(30, 200, 70, 70);
+        c.addActionListener(e -> tfld.setText(""));
 
 
 
 
-        gui.add(a);gui.add(b);gui.add(c);
+        gui.add(tfld);
+        gui.add(c);
+        gui.add(b1);gui.add(b2);gui.add(b3);
         gui.setSize(400, 600);
         gui.setLayout(null);
         gui.setLocationRelativeTo(null);
